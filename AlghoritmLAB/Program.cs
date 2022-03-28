484a6d6075869c116bb8ec40cb216b98a5694b7f
aswds <sasha.iilllnitskiy@gmail.com>
2022-03-28 20:33:17 +0300

    Adding SortArray function

--- AlghoritmLAB/Program.cs
+++ AlghoritmLAB/Program.cs
@@ -295,7 +295,10 @@ namespace AlgorithmsLAB1
                 Console.WriteLine($"\nRunTime(in Milliseconds):{ts.TotalMilliseconds}");
             }
         }
-        public static void SortArray() { }
+        public static void SortArray() {
+            if (true) { 
+            }
+        }
         public static void BinarySearch(int[] array)
         {
             Stopwatch stopWatch = new Stopwatch();
